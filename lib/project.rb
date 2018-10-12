58d96a7d1883e7908c292fc6f71b45711a88d14f
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 23:42:52 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 class Project
-  attr_accessor :name, :backers
+  attr_accessor :title, :backers
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
 
